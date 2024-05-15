2784f06b829a7b771c7da9d7a77afb9c22c90974
unknown <shakthivelmanjeedan11@gmail.com>
2024-05-15 12:32:04 +0530

    Implemented the routing for 4 endpoints with validations

---
+++ backend/routes/taskRouting.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const router = express.Router();
+const Task = require('../models/task');
+
+
+router.post('/tasks', async (req, res) => {
+  try {
+    const task = new Task(req.body);
+    await task.save();
+    res.status(201).send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+
+router.put('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['title', 'description', 'completed'];
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+  
+    if (!isValidOperation) {
+      return res.status(400).send({ error: 'Invalid updates!' });
+    }
+  
+    try {
+      const task = await Task.findById(req.params.id);
+  
+      if (!task) {
+        return res.status(404).send();
+      }
+  
+      updates.forEach(update => task[update] = req.body[update]);
+      await task.save();
+  
+      res.send(task);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  });
+
+
+  router.delete('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+
+router.patch('/tasks/:id/complete', async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    task.completed = true;
+    await task.save();
+    res.send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+module.exports = router;
+
+
